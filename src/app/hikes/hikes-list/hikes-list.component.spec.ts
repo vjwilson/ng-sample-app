2f5bdc3f820b46c9cda298d4f04fecb153405c4e
Van J. Wilson <van.wilson1992@alumni.unc.edu>
2018-01-28 22:15:22 -0500

    Add appendMiles pipe, with some working tests

--- src/app/hikes/hikes-list/hikes-list.component.spec.ts
+++ src/app/hikes/hikes-list/hikes-list.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { StarComponent } from '../../shared/star/star.component';
+import { HikesService } from '../hikes.service';
 
 import { HikesListComponent } from './hikes-list.component';
 
@@ -8,7 +10,11 @@ describe('HikesListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HikesListComponent ]
+      declarations: [
+        HikesListComponent,
+        StarComponent,
+      ],
+      providers: [HikesService],
     })
     .compileComponents();
   }));
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StarComponent } from './star/star.component';
+import { AppendMilesPipe } from './append-miles.pipe';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [StarComponent],
+  declarations: [StarComponent, AppendMilesPipe],
   exports: [
     StarComponent,
+    AppendMilesPipe,
   ],
 })
 export class SharedModule { }
